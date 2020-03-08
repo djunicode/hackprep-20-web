91e5a128784dff8e619d6e5dcad89822861ea2dd
Vikrant Gajria <vikrantgajria@gmail.com>
2020-03-08 11:15:26 +0530

    Add form

--- voices/social/urls.py
+++ voices/social/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('info', views.info),
+    path('', views.index, name='home'),
+    path('new', views.submit_complaint),
 ]
--- voices/social/views.py
+++ voices/social/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Complaint
+from .forms import ComplaintForm
 
 def index(request):
     complaints = Complaint.objects.all()
@@ -7,8 +8,13 @@ def index(request):
         "complaints": complaints,
     })
 
-def info(request):
-    description = "Hello Worl pls im hungry"
-    return render(request, "complaint.html", {
-        "description": description,
-    })
+def submit_complaint(request):
+    if request.method == 'GET':
+        return render(request, "complaint.html", {
+            "form": ComplaintForm,
+        })
+    else:
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='home')
