0f68fc3fe65f2fcc742843306c7f04805e22de57
Vikrant Gajria <vikrantgajria@gmail.com>
2020-03-08 10:54:29 +0530

    Add foreign key

--- voices/social/models.py
+++ voices/social/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Complaint(models.Model):
     heading = models.CharField(max_length=128)
     description = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
